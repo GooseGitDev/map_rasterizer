c8a59d89c3acb884e13ba89f7db162de54a9b0ca
GooseGitDev <dpmail138@yahoo.com>
2026-09-11 12:00:56 -0700

    fix context parameter

--- routers/map_rasterizer.py
+++ routers/map_rasterizer.py
@@ -8,7 +8,8 @@ templates = Jinja2Templates(directory="templates")
 
 @router.get("/", response_class=HTMLResponse)
 async def get_game(request: Request):
-    return templates.TemplateResponse("map_rasterizer.html", {"request": request})
+    return templates.TemplateResponse("map_rasterizer.html", context={"request": request})
+
 
 @router.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
